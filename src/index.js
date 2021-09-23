d12aa469166b50d91d636ebb480819e4d2ca0f12
Miika Pynttäri <77153522+Lagipp@users.noreply.github.com>
2021-09-23 16:37:04 +0300

    added a for-loop to generate multiple elements

--- src/index.js
+++ src/index.js
@@ -2,31 +2,33 @@ import "./styles.css";
 
 let container = document.getElementsByClassName("container");
 
-let newDiv = document.createElement("div");
-newDiv.className = "wiki-item";
+for (let x = 0; x < 5; x++) {
+  let newDiv = document.createElement("div");
+  newDiv.className = "wiki-item";
 
-let newHeader = document.createElement("h1");
-newHeader.className = "wiki-header";
+  let newHeader = document.createElement("h1");
+  newHeader.className = "wiki-header";
 
-let newContent = document.createElement("div");
-newContent.className = "wiki-content";
+  let newContent = document.createElement("div");
+  newContent.className = "wiki-content";
 
-let newParagraph = document.createElement("p");
-newParagraph.className = "wiki-text";
+  let newParagraph = document.createElement("p");
+  newParagraph.className = "wiki-text";
 
-let newImageContainer = document.createElement("div");
-newImageContainer.className = "img-container";
+  let newImageContainer = document.createElement("div");
+  newImageContainer.className = "img-container";
 
-let newImage = document.createElement("img");
-newImage.className = "wiki-img";
+  let newImage = document.createElement("img");
+  newImage.className = "wiki-img";
 
-newImageContainer.appendChild(newImage);
-newContent.appendChild(newParagraph);
-newContent.appendChild(newImageContainer);
-newDiv.appendChild(newHeader);
-newDiv.appendChild(newContent);
+  newImageContainer.appendChild(newImage);
+  newContent.appendChild(newParagraph);
+  newContent.appendChild(newImageContainer);
+  newDiv.appendChild(newHeader);
+  newDiv.appendChild(newContent);
 
-container.appendChild(newDiv);
+  container.appendChild(newDiv);
+}
 
 //document.body.appendChild(container);
 
