0bb759e973542f94c3445952394b549292839e51
Miika Pynttäri <77153522+Lagipp@users.noreply.github.com>
2021-09-23 16:21:21 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import "./styles.css";
+
+let container = document.createElement("div");
+container.className = "container";
+
+let newDiv = document.createElement("div");
+newDiv.className = "wiki-item";
+
+let newHeader = document.createElement("h1");
+newHeader.className = "wiki-header";
+
+let newContent = document.createElement("div");
+newContent.className = "wiki-content";
+
+let newParagraph = document.createElement("p");
+newParagraph.className = "wiki-text";
+
+let newImageContainer = document.createElement("div");
+newImageContainer.className = "img-container";
+
+let newImage = document.createElement("img");
+newImage.className = "wiki-img";
+
+newImageContainer.appendChild(newImage);
+newContent.appendChild(newParagraph);
+newContent.appendChild(newImageContainer);
+newDiv.appendChild(newHeader);
+newDiv.appendChild(newContent);
+
+container.appendChild(newDiv);
+
+document.body.appendChild(container);
+
+/*
+if (document.readyState !== "loading") {
+  initializeCode();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    initializeCode();
+  });
+}
+
+function initializeCode() {
+  document.getElementById("app").innerHTML = "<h1>Hello!</h1>";
+}
+*/
